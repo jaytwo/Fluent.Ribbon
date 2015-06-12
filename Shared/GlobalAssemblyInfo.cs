87995cfa1856eac4a081149cbf9d2458b34ae575
Tim Jones <tim@timjones.tw>
2015-06-12 16:29:34 +0800

    Bump version number

--- Shared/GlobalAssemblyInfo.cs
+++ Shared/GlobalAssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.4.0.0")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyVersion("3.4.0.1")]
+[assembly: AssemblyFileVersion("3.4.0.1")]
 [assembly: AssemblyInformationalVersion("SRC")]
 
 [assembly: ComVisible(false)]
